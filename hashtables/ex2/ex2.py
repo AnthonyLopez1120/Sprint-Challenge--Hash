64941807d4f75fa2cae6bdd96d8bd017dfc67c89
Anthony Lopez <anthonybeconscious@gmail.com>
2020-05-08 12:15:28 -0500

    mvp

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,4 +1,4 @@
-#  Hint:  You may not need all of these.  Remove the unused functions.
+#  Hint:  You may not need all of these  Remove the unused functions.
 class Ticket:
     def __init__(self, source, destination):
         self.source = source
@@ -10,5 +10,14 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    cache = {}
+    for i in tickets:
+        cache[i.source] = i.destination
+    route = [None] * length
+    cur_source = ''
+    for i in range(length):
+        route[i] = cache[cur_source]
+        cur_source = cache[cur_source]
+
 
     return route
