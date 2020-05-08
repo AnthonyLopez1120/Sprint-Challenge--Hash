89b44387a1fef0081bfbc37b73c50fea9b813a35
Anthony Lopez <anthonybeconscious@gmail.com>
2020-05-08 10:58:44 -0500

    one is done son

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,11 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    cache = {}
+    for i in range(len(weights)):
+        if limit - weights[i] in cache:
+            return [i, cache[limit - weights[i]]]
+        if weights[i] not in cache:
+            cache[weights[i]] = i
 
     return None
